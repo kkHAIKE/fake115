efcccc91b00c8c6e3b929d757c892d10d531f80e
MagicDroidX <magicdroidx@gmail.com>
2016-09-15 12:00:45 +0800

    Update fake115.user.js

--- fake115.user.js
+++ fake115.user.js
@@ -4,7 +4,7 @@
 // @version      1.1
 // @description  非115浏览器登录115.com
 // @author       kkhaike
-// @match        http://115.com/*
+// @match        https://115.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @grant        GM_log
